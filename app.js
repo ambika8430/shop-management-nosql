5580979dbb8683f3a0d478d26d3f4fdc770f8faf
ambika8430 <grey.4e2@gmail.com>
2025-04-07 12:04:30 +0530

    final commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
+require('dotenv').config();
 const path = require('path');
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -7,50 +7,41 @@ const mongoose = require('mongoose');
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
+  User.findById('67f3554dcdad8a1b457714c1')
     .then(user => {
+      if (!user) return next();
       req.user = user;
       next();
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      console.log('Error fetching user:', err);
+      next();
+    });
 });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-
 app.use(errorController.get404);
 
-mongoose
-  .connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
-  )
-  .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'Max',
-          email: 'max@test.com',
-          cart: {
-            items: []
-          }
-        });
-        user.save();
-      }
+mongoose.connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log('Connected to MongoDB via Mongoose');
+    app.listen(3000, () => {
+      console.log('Server is running on port 3000');
     });
-    app.listen(3000);
   })
   .catch(err => {
-    console.log(err);
+    console.log('Mongoose connection failed:', err.message);
   });
